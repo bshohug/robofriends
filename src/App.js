48305edff74e927900aa8b88f616be5c0c62c20d
BM Mahamudul Hasan Shohug <55352679+bshohug@users.noreply.github.com>
2023-03-30 14:57:45 +0600

    updating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -38,7 +38,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filterRobots} />
+          <Scroll>
+            <CardList robots={filterRobots} />
+          </Scroll>
         </div>
       );
     }
